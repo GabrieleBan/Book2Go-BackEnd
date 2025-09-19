62e03dc07fbfba83b220239b351e0c5d3b2e6ee3
Emanuele <emanuelespadaro1@gmail.com>
2025-09-19 16:33:27 +0200

    chore(tests): faker dependency

--- tests/test/sample.spec.ts
+++ tests/test/sample.spec.ts
@@ -1,7 +1,15 @@
 import { expect } from 'chai';
 
+import { fakerIT as faker } from '@faker-js/faker';
+
 describe('Sample Test', () => {
     it('should pass', () => {
+        const randomName = faker.person.fullName(); // Rowan Nikolaus
+        const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+
+        console.log(`Random Name: ${randomName}`);
+        console.log(`Random Email: ${randomEmail}`);
         expect(true).to.be.true;
     });
 });
+ 
