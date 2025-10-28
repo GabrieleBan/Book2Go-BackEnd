7701b6da95d2566b71639aa946c6911612479036
Gabriele Banchio <gabriele.banchio2001@gmail.com>
2025-10-28 18:04:11 +0100

    chore(recommendation) added controller and SecurityConfig WIP

---
+++ recommendation-service/src/test/java/com/b2g/recomendationservice/RecomendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2g.recomendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecomendationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
