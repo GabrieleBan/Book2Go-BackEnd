386a232d621755d702e4d49d24c6609f5c885fd4
Gabriele Banchio <gabriele.banchio2001@gmail.com>
2025-11-21 16:58:15 +0100

    chore(project structure) : last commit before migration

---
+++ reader-service/src/test/java/com/b2g/readerservice/ReaderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b2g.readerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReaderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
